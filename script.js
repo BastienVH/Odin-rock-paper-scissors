cb1d851c37c1f58dce3b0a3ef4458c51fdbcd2a3
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-26 20:01:23 +0100

    defined algorithm for computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("It's alive!!!");
+function computerPlay() {
+  // generate random number between 1 and 3
+  // create a variable that will hold the computers selection 
+  // switch 1 = rock, 2 = paper, 3 = scissors
+  // return selection
+}
