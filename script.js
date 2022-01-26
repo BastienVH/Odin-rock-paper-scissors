e7f6baece8ac9b84b23c8b5a1ceae35b5d3da7f3
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-26 22:50:18 +0100

    Implemented function for computers choice

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ Runthrough of the program (1 iteration):
 
 function computerPlay() {
   // generate random number between 1 and 3
+  let randomNum = Math.floor(Math.random()*3) + 1;
   // create a variable that will hold the computers selection 
+  let computerWeapon = ""
   // switch 1 = rock, 2 = paper, 3 = scissors
+  switch (randomNum) {
+    case 1:
+      computerWeapon = "Rock";
+      break;
+    case 2:
+      computerWeapon = "Paper";
+      break;
+    case 3:
+      computerWeapon = "Scissors";
+      break;
+  }
   // return selection
+  return(computerWeapon);
 }
