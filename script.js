e4d07f376d9357e616795e92eb41981c786c9d38
Bastien Van Houdt <bastienvanhoudt@gmail.com>
2022-01-26 22:45:09 +0100

    layed out flow of program

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+Runthrough of the program (1 iteration):
+- ask user for input (rock, paper, scissors)
+- verify input is correct
+- let computer randomly select between rock, paper and scissors
+- compare user selection and computer selection to return winner
+- output chosen weapons and winner
+*/
+
+
 function computerPlay() {
   // generate random number between 1 and 3
   // create a variable that will hold the computers selection 
